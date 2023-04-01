97384378509325d6ae06c4adbe063449a96f7976
Federico Calvette <calvettefederico@gmail.com>
2023-03-31 23:48:38 -0300

    Black

--- main.py
+++ main.py
@@ -7,36 +7,36 @@ import xmltodict
 FECHA = str(datetime.now())
 
 MONEDA_ISO = {
-    'US.D': ('USD', 'Dolar'),
-    'ARGP': ('ARG', 'Peso Argentino'),
-    'CRUZ': ('BRL', 'Real'),
-    'EUR': ('EUR', 'Euro'),
-    'URGI': ('UYI', 'Unidad Indexada'),
+    "US.D": ("USD", "Dolar"),
+    "ARGP": ("ARG", "Peso Argentino"),
+    "CRUZ": ("BRL", "Real"),
+    "EUR": ("EUR", "Euro"),
+    "URGI": ("UYI", "Unidad Indexada"),
 }
 
 COTIZACION = {
-    'cotizacion':{},
-    'informacion':{
-        'mensaje': 'Estas cotizaciones son en pesos uruguayos y solo a titulo informativo',
-        'fecha_actualizacion': FECHA
-    }
+    "cotizacion": {},
+    "informacion": {
+        "mensaje": "Estas cotizaciones son en pesos uruguayos y solo a titulo informativo",
+        "fecha_actualizacion": FECHA,
+    },
 }
 
-respuesta = requests.get('https://www.itau.com.uy/inst/aci/cotiz.xml')
+respuesta = requests.get("https://www.itau.com.uy/inst/aci/cotiz.xml")
 
 respuesta_diccionario = xmltodict.parse(respuesta.content)
 
-lista_cotizaciones = respuesta_diccionario['root']['cotizacion']
+lista_cotizaciones = respuesta_diccionario["root"]["cotizacion"]
 
 for cotizacion in lista_cotizaciones:
 
-    if cotizacion['moneda'] in MONEDA_ISO:
+    if cotizacion["moneda"] in MONEDA_ISO:
 
-        COTIZACION['cotizacion'][MONEDA_ISO[cotizacion['moneda']][0]] = {
-            'nombre': MONEDA_ISO[cotizacion['moneda']][1],
-            'iso': MONEDA_ISO[cotizacion['moneda']][0],
-            'compra': cotizacion['compra'],
-            'venta': cotizacion['venta']
+        COTIZACION["cotizacion"][MONEDA_ISO[cotizacion["moneda"]][0]] = {
+            "nombre": MONEDA_ISO[cotizacion["moneda"]][1],
+            "iso": MONEDA_ISO[cotizacion["moneda"]][0],
+            "compra": cotizacion["compra"],
+            "venta": cotizacion["venta"],
         }
 
-print(json.dumps(COTIZACION, sort_keys=False, indent=4))    
+print(json.dumps(COTIZACION, sort_keys=False, indent=4))
